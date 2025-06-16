4f14631d90f8096c63e9b6fde0f5367232bf7a57
RhysHub <72181892+RhysHub@users.noreply.github.com>
2025-06-16 19:41:54 +1000

    New logo and shit

--- src/Banner.tsx
+++ src/Banner.tsx
@@ -5,11 +5,11 @@ import Particles from '../Backgrounds/Particles/Particles'
 export default function BannerImage() {
   return (
     <div style={{
-        width: '100%', 
-        height: '100vh',
-        position: 'relative',
-        overflow: 'hidden',
-      }}>
+      width: '100%',
+      height: '100vh',
+      position: 'relative',
+      overflow: 'hidden',
+    }}>
       <Particles
         particleColors={['#FFF', '#AAA']}
         particleCount={200}
@@ -21,44 +21,55 @@ export default function BannerImage() {
         disableRotation={false}
       />
       <div
-      style={{
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: '100%',
-        height: '100%',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        justifyContent: 'center',
-        textAlign: 'center',
-      }}
-    >
-      <CardMedia
-        component="img"
-        alt="Banner Image"
-        image="profile.png"
-        sx={{
-          width: '250px',
-          borderRadius: 8,
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          width: '100%',
+          height: '100%',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          textAlign: 'center',
         }}
-      />
-      <Typography sx={{
-        textShadow: '8px 8px 8px rgba(0, 0, 0, 1)',
-        fontWeight: '600'
-      }} variant="h1" color="text.primary">
-        Real Loose Game Concepts
-      </Typography>
-      <Spacer />
-      <Typography sx={{
-        textShadow: '8px 8px 8px rgba(0, 0, 0, 1)',
-        fontWeight: '300',
-        width:"70%"
-      }} variant="h5" color="text.primary">
-        A group of friends building games
-      </Typography>
-      <Spacer />
-    </div>
+      >
+        <CardMedia
+          component="img"
+          alt="Banner Image"
+          image="7.svg"
+          sx={{
+            width: '1000px',
+            height: '300px',
+            borderRadius: 8,
+          }}
+        />
+        <Typography
+          sx={{
+            fontFamily: '"Staatliches", sans-serif',
+            textShadow: '8px 8px 8px rgba(0, 0, 0, 1)',
+            fontWeight: '400',
+            textTransform: 'uppercase',
+            fontSize: '4.5rem',
+          }}
+          variant="h1"
+          color="text.primary"
+        >
+          <span style={{ color: '#ff3131' }}>R</span>EAL&nbsp;
+          <span style={{ color: '#ff3131' }}>L</span>OOSE&nbsp;
+          <span style={{ color: '#ff3131' }}>G</span>AME&nbsp;
+          <span style={{ color: '#ff3131' }}>CO</span>NCEPTS
+        </Typography>
+        <Spacer />
+        <Typography sx={{
+          textShadow: '8px 8px 8px rgba(0, 0, 0, 1)',
+          fontWeight: '300',
+          width: "70%"
+        }} variant="h5" color="text.primary">
+          Real loose....
+        </Typography>
+        <Spacer />
+      </div>
     </div>
   );
 }
